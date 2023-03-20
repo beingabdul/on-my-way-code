2b02cc873455a0c86b81b8dd84d85d7c4ed94152
amy1q <silasamy2424@gmail.com>
2023-03-20 21:44:51 +0500

    firebase.js

---
+++ public/js/firebase.js
@@ -0,0 +1,30 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js"
+import * as fbauth from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js"
+import * as fbstorage from "https://www.gstatic.com/firebasejs/9.18.0/firebase-storage.js"
+import * as fbfirestore from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js"
+
+var firebaseConfig = {
+    
+}
+// Initialize Firebase
+export  const app = initializeApp(firebaseConfig)
+export let auth = fbauth
+export let db = fbfirestore.getFirestore(app)
+export let doc = fbfirestore.doc
+export let getDoc = fbfirestore.getDoc
+export let storage = fbstorage
+export let getDocs = fbfirestore.getDocs
+export let query = fbfirestore.query
+export let where = fbfirestore.where
+export let orderBy = fbfirestore.orderBy
+export let setdoc = fbfirestore.setDoc
+export let timestamp = fbfirestore.Timestamp
+export let collection = fbfirestore.collection
+export let addDoc = fbfirestore.addDoc
+export let setDoc = fbfirestore.setDoc
+export let updateDoc = fbfirestore.updateDoc
+export let deleteDoc = fbfirestore.deleteDoc
+export let serverTimestamp = fbfirestore.serverTimestamp
+export let onSnapshot = fbfirestore.onSnapshot
+export let limit = fbfirestore.limit
